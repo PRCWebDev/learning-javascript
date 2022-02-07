e9b8cb2cdc11bbbc20cd2d4da709d527d8fdbbca
PRCWebDev <webdevprc@gmail.com>
2022-02-07 17:17:03 +0200

    challenge #2 completed

---
+++ js-challenges/02/script.js
@@ -0,0 +1,21 @@
+// const weightMark = 78;
+// const heightMark = 1.69;
+const weightMark = 95;
+const heightMark = 1.88;
+const bmiMark = weightMark / heightMark ** 2;
+
+// const weightJohn = 92;
+// const heightJohn = 1.95;
+const weightJohn = 85;
+const heightJohn = 1.76;
+const bmiJohn = weightJohn / heightJohn ** 2;
+
+const markHigherBMI = bmiMark > bmiJohn;
+
+console.log(bmiMark, bmiJohn, markHigherBMI);
+
+if (markHigherBMI) {
+  console.log(`Mark's BMI(${bmiMark}) is higher than John's(${bmiJohn})!`);
+} else {
+  console.log(`John's BMI(${bmiJohn}) is higher than Mark's(${bmiMark})!`);
+}
