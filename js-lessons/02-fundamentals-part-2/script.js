87161b6a0b2ece1d8fe6a772326cce690c0dac32
PRCWebDev <webdevprc@gmail.com>
2022-02-10 10:43:31 +0200

    learned a bit about js functions

--- js-lessons/02-fundamentals-part-2/script.js
+++ js-lessons/02-fundamentals-part-2/script.js
@@ -1,5 +1,5 @@
 "use strict";
-// /*
+/*
 ///////////////////////////////////////
 // Activating Strict Mode
 let hasDriversLicense = false;
@@ -12,3 +12,29 @@ if (hasDriversLicense) console.log("I can drive :D");
 // future reserved words
 // const interface = 'Audio';
 // const private = 534;
+*/
+
+///////////////////////////////////////
+// Functions
+function logger() {
+  console.log("My name is Radu");
+}
+
+// calling / running / invoking function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+  // 'apples' and 'oranges' are called parameters
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+  return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0); // '5' and '0' are called arguments
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+
+const num = Number("23");
