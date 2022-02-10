f45c622777c58c8d99fbc180a00edb75434f31ef
PRCWebDev <webdevprc@gmail.com>
2022-02-10 10:01:07 +0200

    learned about js strict mode and modified Readme

---
+++ js-lessons/02-fundamentals-part-2/script.js
@@ -0,0 +1,14 @@
+"use strict";
+// /*
+///////////////////////////////////////
+// Activating Strict Mode
+let hasDriversLicense = false;
+const passTest = true;
+
+// if (passTest) hasDriverLicense = true;
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log("I can drive :D");
+
+// future reserved words
+// const interface = 'Audio';
+// const private = 534;
