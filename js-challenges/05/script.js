153e8bc3ad65186023eb6f04773ab42435c60b60
PRCWebDev <webdevprc@gmail.com>
2022-02-12 12:30:29 +0200

    completed challenge #5 based on the Functions theory lectures

---
+++ js-challenges/05/script.js
@@ -0,0 +1,30 @@
+"use strict";
+
+// TEST DATA 1: Dolphins score 44, 23 and 71. Koalas score 65, 54 and 49
+// TEST DATA 2: Dolphins score 85, 54 and 41. Koalas score 23, 34 and 27
+
+const calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3;
+
+// Test 1
+let scoreDolphins = calcAverage(44, 23, 71);
+let scoreKoalas = calcAverage(65, 54, 49);
+console.log(scoreDolphins, scoreKoalas);
+
+const checkWinner = function (avgDolphins, avgKoalas) {
+  if (avgDolphins >= 2 * avgKoalas) {
+    console.log(`Dolphins win (${avgDolphins} vs ${avgKoalas})`);
+  } else if (avgKoalas >= 2 * avgDolphins) {
+    console.log(`Koalas win (${avgKoalas} vs ${avgDolphins})`);
+  } else {
+    console.log("No team wins");
+  }
+};
+
+checkWinner(scoreDolphins, scoreKoalas);
+// checkWinner(100, 50); // tested with other values
+
+// Test 1
+scoreDolphins = calcAverage(85, 54, 41);
+scoreKoalas = calcAverage(23, 34, 27);
+console.log(scoreDolphins, scoreKoalas);
+checkWinner(scoreDolphins, scoreKoalas);
