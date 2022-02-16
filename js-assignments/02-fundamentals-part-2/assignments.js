66aac5a22dabd3693985e935b400217f3d1ec6d9
PRCWebDev <webdevprc@gmail.com>
2022-02-16 13:20:22 +0200

    learnead about Looping Backwards and Loops in Loops & completed 1 assignment

--- js-assignments/02-fundamentals-part-2/assignments.js
+++ js-assignments/02-fundamentals-part-2/assignments.js
@@ -163,3 +163,17 @@ for (let i = 0; i < populations.length; i++) {
   percentages2.push(perc);
 }
 console.log(percentages2);
+
+////////////////////////////////////
+// LECTURE: Looping Backwards and Loops in Loops
+const listOfNeighbours = [
+  ["Canada", "Mexico"],
+  ["Spain"],
+  ["Norway", "Sweden", "Russia"],
+];
+
+for (let i = 0; i < listOfNeighbours.length; i++) {
+  for (let y = 0; y < listOfNeighbours[i].length; y++) {
+    console.log(`Neighbour: ${listOfNeighbours[i][y]}`);
+  }
+}
