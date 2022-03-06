eae8442aee8d95659aa5243722b577810208db29
PRCWebDev <webdevprc@gmail.com>
2022-03-06 21:30:58 +0200

    learned more about event listeners, working and manipulating classes in JS and almost completed a new mini-project

---
+++ js-lessons/06-modal-window/script.js
@@ -0,0 +1,69 @@
+"use strict";
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const btnsOpenModal = document.querySelectorAll(".show-modal");
+
+// console.log(btnsOpenModal);
+
+// for (let i = 0; i < btnsOpenModal.length; i++) {
+//   console.log(btnsOpenModal[i].textContent);
+// }
+
+// *** 1. Displaying / opening the modal window and the overlay
+/*
+// 1 - V1
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener("click", function () {
+    console.log(`Button ${i + 1} clicked`);
+    modal.classList.remove("hidden"); // this is the same as:
+    // modal.style.display = "block";
+    overlay.classList.remove("hidden");
+  });
+}
+*/
+
+// 1 - V2 - BETTER
+const openModal = function () {
+  console.log("Show modal button clicked");
+  modal.classList.remove("hidden"); // this is the same as:
+  // modal.style.display = "block";
+  overlay.classList.remove("hidden");
+};
+
+// displaying / opening the modal window and the overlay
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener("click", openModal);
+}
+// we DON'T call the function, we just instruct the JS engine to call the function once the "click" happens
+
+// *** 2. Hidding / closing the modal window and the overlay
+/*
+// 2 - V1
+btnCloseModal.addEventListener("click", function () {
+  console.log("Modal closed");
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+});
+
+// hidding / closing the overlay
+overlay.addEventListener("click", function () {
+  console.log("Clicked outside of the modal window to close the modal window");
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+});
+*/
+
+// 2 - V2 - BETTER
+const closeModal = function () {
+  console.log("Close modal button clicked");
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+// hidding / closing the modal window
+btnCloseModal.addEventListener("click", closeModal); // we DON'T call the function, we just instruct the JS engine to call the function once the "click" happens
+
+// hidding / closing the overlay
+overlay.addEventListener("click", closeModal); // we DON'T call the function, we just instruct the JS engine to call the function once the "click" happens
