6493cd60c96f1c9c564fc63d47b12f56e5a23fce
PRCWebDev <webdevprc@gmail.com>
2022-04-13 07:15:38 +0300

    completed a challenge about Looping Arrays and Objects

--- js-challenges/10/script.js
+++ js-challenges/10/script.js
@@ -41,24 +41,24 @@ const game = {
   },
 };
 
-// 1.
+// 1. Create one player array for each team (variables 'players1' and 'players2')
 const [players1, players2] = game.players;
 console.log(players1, players2);
 
-// 2.
+// 2. The first player in any player array is the goalkeeper and the others are field players. For Bayern Munich (team 1) create one variable ('gk') with the goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10 field players
 const [gk, ...fieldPlayers] = players1; // using the REST Operator (left side of "=")
 console.log(gk);
 console.log(fieldPlayers);
 
-// 3.
+// 3. Create an array 'allPlayers' containing all players of both teams (22 players)
 const allPlayers = [...players1, ...players2]; // using the SPREAD Operator (right side of "=")
 console.log(allPlayers);
 
-// 4.
+// 4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a new array ('players1Final') containing all the original team1 players plus 'Thiago', 'Coutinho' and 'Perisic'
 const players1Final = [...players1, "Thiago", "Coutinho", "Perisic"];
 console.log(players1Final);
 
-// 5.
+// 5. Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and 'team2')
 // Destructuring Nested Objects
 const {
   odds: { team1, x: draw, team2 },
@@ -67,16 +67,19 @@ const {
 // const { team1, x: draw, team2 } = game.odds;
 console.log(team1, draw, team2);
 
-// 6.
+// 6. Write a function ('printGoals') that receives an arbitrary number of player names (NOT an array) and prints each of them to the console, along with the number of goals that were scored in total (number of player names passed in)
 // using REST Parameters
 const printGoals = function (...players) {
-  console.log(players);
+  for (const player of players) {
+    console.log(player);
+  }
   console.log(`${players.length} goals were scored.`);
 };
 
 // printGoals("Davies", "Muller", "Lewandowski", "Kimmich");
+// printGoals("Davies", "Muller");
 printGoals(...game.scored); // using the SPREAD Operator
 
-// 7.
+// 7. The team with the lower odd is more likely to win. Print to the console which team is more likely to win, WITHOUT using an if/else statement or the ternary operator.
 team1 < team2 && console.log(`${game.team1} wins the game.`);
 team1 > team2 && console.log(`${game.team2} wins the game.`);
