99b7d4f2cdd9a909b0edbedbe6c8cdb6ec88771c
PRCWebDev <webdevprc@gmail.com>
2022-04-15 21:45:29 +0300

    learned a bit about Working with Strings & practiced what I learned

--- js-lessons/09-data-structures-operators/script.js
+++ js-lessons/09-data-structures-operators/script.js
@@ -54,10 +54,6 @@ const testRadu = [
 console.log(testRadu);
 */
 
-// Data needed for a later exercise
-const flights =
-  "_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30";
-
 // Data needed for first part of the section
 const restaurant = {
   name: "Classico Italiano",
@@ -917,3 +913,187 @@ console.log([...question.values()]);
 console.log(...question.entries());
 console.log(...question.keys());
 console.log(...question.values());
+
+///////////////////////////////////////
+// 14. Working with STRINGS
+const flights =
+  "_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30";
+
+const airline = "TAP Air Portugal";
+const plane = "A320";
+
+console.log(plane[0]);
+console.log(plane[1]);
+console.log(plane[2]);
+console.log("B737"[0]);
+
+console.log(airline.length);
+console.log("B737".length);
+
+console.log(airline.indexOf("r")); // 6 - the space counts as character - count starting from position 0 just like in Arrays
+console.log(airline.lastIndexOf("r")); // 10 - the space counts as character - count starting from position 0 just like in Arrays
+console.log(airline.indexOf("portugal")); // -1 - "portugal" does NOT exist - CASE SENSITIVE
+console.log(airline.indexOf("Portugal")); // 8 - "Portugal" is on position 8 - count starting from position 0 just like in Arrays
+
+console.log(airline.slice(4));
+console.log(airline.slice(4, 7));
+
+console.log(airline.slice(0, airline.lastIndexOf(" ")));
+// console.log(airline.slice(airline.indexOf(" ") + 1, airline.lastIndexOf(" ")));
+console.log(airline.slice(airline.lastIndexOf(" ") + 1));
+
+console.log(airline.slice(-2));
+console.log(airline.slice(1, -1));
+
+const checkMiddleSeat = function (seat) {
+  // B and E are middle seats
+  const s = seat.slice(-1);
+  if (s === "B" || s === "E") {
+    console.log("You got the middle seat 😬");
+  } else {
+    console.log("You got lucky 😎");
+  }
+};
+checkMiddleSeat("11B");
+checkMiddleSeat("23C");
+checkMiddleSeat("3E");
+
+// STRINGS are just PRIMITIVES.
+// So why do they have methods?
+// Whenever we call a method on a string, JavaScript will automatically, behind the scenes, CONVERT that String Primitive to a String OBJECT with the same content.
+// And then it's on that OBJECT where the Methods are CALLED.
+// This process is called BOXING because it basically takes our String and puts it into a BOX which is the OBJECT.
+// When the operation is done the Object is converted back to a regular String Primitive.
+console.log(new String("jonas"));
+console.log(typeof new String("jonas"));
+console.log(typeof new String("jonas").slice(1));
+
+console.log(airline.toLowerCase());
+console.log(airline.toUpperCase());
+
+// Fixing capitalization in name
+// !! MUST DO CHALLENGE - Turn into a function
+const passenger = "jOnAS"; // Jonas
+const passengerLower = passenger.toLowerCase();
+const passengerCorrect =
+  passengerLower[0].toUpperCase() + passengerLower.slice(1);
+console.log(passengerCorrect); // Jonas
+
+// Comparing emails
+// !! MUST DO CHALLENGE - Turn into a function
+const email = "hello@jonas.io";
+const loginEmail = "  Hello@Jonas.Io \n";
+
+// const lowerEmail = loginEmail.toLowerCase();
+// const trimmedEmail = lowerEmail.trim();
+const normalizedEmail = loginEmail.toLowerCase().trim();
+console.log(normalizedEmail);
+console.log(email === normalizedEmail);
+
+// Replacing
+// !! MUST DO CHALLENGE - Turn into a function
+const priceGB = "288,97£";
+const priceUS = priceGB.replace("£", "$").replace(",", ".");
+console.log(priceUS);
+
+const announcement =
+  "All passengers come to boarding door 23. Boarding door 23!";
+
+console.log(announcement.replace("door", "gate")); // ONLY replaces the FIRST occurence of "door" with gate"
+// console.log(announcement.replace(/gate/g, "door")); // older Method using Regular Expression - used before ".replaceAll();" - also Case Sensitive
+console.log(announcement.replaceAll("door", "gate")); // the ".replaceAll();" Method is NOW WORKING
+
+// Booleans
+const plane2 = "Airbus A320neo";
+console.log(plane2.includes("A320"));
+console.log(plane2.includes("Boeing"));
+console.log(plane2.startsWith("Airb"));
+
+if (plane2.startsWith("Airbus") && plane2.endsWith("neo")) {
+  console.log("Part of the NEW ARirbus family");
+}
+
+// Practice exercise
+const checkBaggage = function (items) {
+  const baggage = items.toLowerCase();
+
+  if (baggage.includes("knife") || baggage.includes("gun")) {
+    console.log("You are NOT allowed on board");
+  } else {
+    console.log("Welcome aboard!");
+  }
+};
+
+checkBaggage("I have a laptop, some Food and a pocket Knife");
+checkBaggage("Socks and camera");
+checkBaggage("Got some snacks and a gun for protection");
+// Split and join
+// SPLIT returns an ARRAY from a STRING
+// we can LOOP over that Array
+console.log("a+very+nice+string".split("+"));
+console.log("Jonas Schmedtmann".split(" "));
+
+const [firstName2, lastName2] = "Jonas Schmedtmann".split(" ");
+
+// SPLIT returns a STRING from an ARRAY
+const newName = ["Mr.", firstName2, lastName2.toUpperCase()].join(" ");
+console.log(newName);
+const newName2 = ["Mr.", firstName2, lastName2.toUpperCase()].join("-");
+console.log(newName2);
+const newName3 = ["Mr.", firstName2, lastName2.toUpperCase()].join("*");
+console.log(newName3);
+
+const capitalizeName = function (name) {
+  const names = name.split(" ");
+  const namesUpper = [];
+
+  for (const n of names) {
+    // namesUpper.push(n[0].toUpperCase() + n.slice(1));
+    // OR
+    namesUpper.push(n.replace(n[0], n[0].toUpperCase()));
+  }
+  console.log(namesUpper.join(" "));
+};
+
+capitalizeName("jessica ann smith davis");
+capitalizeName("jonas schmedtmann");
+
+// Extra - Radu
+const anyArray = [
+  "a",
+  "b",
+  "c",
+  " ",
+  3,
+  4,
+  5,
+  " ",
+  true,
+  "_no_space_",
+  [1, 2],
+  " ",
+  // { key: "value" }, - NOT WORKING on Objects
+];
+const arrayToString = anyArray.join("");
+console.log(arrayToString);
+
+// RADU - test Accesa
+function hideDigits(num) {
+  var maskedString = "";
+  // Write code that returns markedString as the answer
+  maskedString = [];
+  for (let i = 0; i < num.length; i++) {
+    if (i < num.length - 3) {
+      maskedString.push("#");
+    } else {
+      maskedString.push(num[i]);
+    }
+  }
+  maskedString = maskedString.join("");
+  console.log(maskedString);
+
+  return maskedString;
+}
+
+hideDigits("1234567890");
+// hideDigits("a1s2d3");
