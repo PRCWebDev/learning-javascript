3826bb4db9034e210762dc235e54fad66c6eed77
PRCWebDev <webdevprc@gmail.com>
2022-04-21 19:58:33 +0300

    learned a bit more about How Passing Arguments Works: Values vs. Reference & Callback Functions

--- js-lessons/10-more-about-functions/script.js
+++ js-lessons/10-more-about-functions/script.js
@@ -30,3 +30,69 @@ createBooking("LH123", 2);
 createBooking("LH123", 5);
 
 createBooking("LH123", undefined, 1000); // skipping a Default Parameter by setting it to "undefined" when we CALL the function
+
+///////////////////////////////////////
+// 2. How Passing Arguments Works: Values vs. Reference
+const flight = "LH234";
+const jonas = {
+  name: "Jonas Schmedtmann",
+  passport: 24739479284,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = "LH999";
+  passenger.name = "Mr. " + passenger.name;
+
+  if (passenger.passport === 24739479284) {
+    // alert("Checked in");
+  } else {
+    // alert("Wrong passport!");
+  }
+};
+// This...
+// checkIn(flight, jonas);
+// console.log(flight);
+// console.log(jonas);
+
+// ... is the same as doing this:
+// const flightNum = flight;
+// const passenger = jonas;
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 100000000000);
+};
+
+newPassport(jonas);
+checkIn(flight, jonas);
+
+///////////////////////////////////////
+// 3. Functions Accepting Callback Functions
+
+const oneWord = function (str) {
+  return str.replaceAll(" ", "").toLowerCase();
+};
+// console.log(oneWord("JavaScript is the best!"));
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(" "); // "..." = the REST Operator used here to PACK all of the remaining words into one Array
+  return [first.toUpperCase(), ...others].join(" "); // "..." = the SPREAD Operator used here to UNPACK the "others" Array created earlier with the Rest Operator
+};
+// console.log(upperFirstWord("JavaScript is the best!"));
+
+// High-order function
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+
+  console.log(`Transformed by: ${fn.name}`);
+};
+
+transformer("JavaScript is the best!", upperFirstWord);
+transformer("JavaScript is the best!", oneWord);
+
+// JS uses CALLBACK Functions all the time
+const high5 = function () {
+  console.log("👋");
+};
+document.body.addEventListener("click", high5);
+["Jonas", "Martha", "Adam"].forEach(high5);
