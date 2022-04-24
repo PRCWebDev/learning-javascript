1246cfca5aeefccb20e6e2de5ddc80a64461ef8a
PRCWebDev <webdevprc@gmail.com>
2022-04-24 22:16:11 +0300

    completed a very short coding challenge

--- js-lessons/10-more-about-functions/script.js
+++ js-lessons/10-more-about-functions/script.js
@@ -71,13 +71,13 @@ checkIn(flight, jonas);
 const oneWord = function (str) {
   return str.replaceAll(" ", "").toLowerCase();
 };
-console.log(oneWord("JavaScript is the best!"));
+// console.log(oneWord("JavaScript is the best!"));
 
 const upperFirstWord = function (str) {
   const [first, ...others] = str.split(" "); // "..." = the REST Operator used here to PACK all of the remaining words into one Array
   return [first.toUpperCase(), ...others].join(" "); // "..." = the SPREAD Operator used here to UNPACK the "others" Array created earlier with the Rest Operator
 };
-console.log(upperFirstWord("JavaScript is the best!"));
+// console.log(upperFirstWord("JavaScript is the best!"));
 
 // 4. HIGHER-ORDER Function
 const transformer = function (str, fn) {
@@ -246,7 +246,7 @@ console.log(addVAT2(100));
 console.log(addVAT2(23));
 
 ///////////////////////////////////////
-// Coding Challenge #14
+// Coding Challenge #1
 /* 
 Let's build a simple poll app!
 
@@ -424,5 +424,24 @@ const boardPassengers = function (n, wait) {
 const perGroup = 1000; // PROOF that the CLOSURE has PRIORITY over the Scope Chain
 boardPassengers(180, 3);
 
-/*
- */
+///////////////////////////////////////
+// Coding Challenge #2
+
+/* 
+This is more of a thinking challenge than a coding challenge 🤓
+
+Take the IIFE below and at the end of the function, attach an event listener that changes the color of the selected h1 element ('header') to blue, each time the BODY element is clicked. Do NOT select the h1 element again!
+
+And now explain to YOURSELF (or someone around you) WHY this worked! Take all the time you need. Think about WHEN exactly the callback function is executed, and what that means for the variables involved in this example.
+
+GOOD LUCK 😀
+*/
+
+(function () {
+  const header = document.querySelector("h1");
+  header.style.color = "red";
+
+  document.querySelector("body").addEventListener("click", function () {
+    header.style.color = "blue";
+  });
+})();
