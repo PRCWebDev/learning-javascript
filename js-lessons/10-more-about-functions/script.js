72570d9ce8c0c30f6040131b7b824fa7c78cd036
PRCWebDev <webdevprc@gmail.com>
2022-04-24 22:02:59 +0300

    completed another coding challenge & learned about Immediately Invoked Function Expressions (IIFE) and Closures

--- js-lessons/10-more-about-functions/script.js
+++ js-lessons/10-more-about-functions/script.js
@@ -71,15 +71,15 @@ checkIn(flight, jonas);
 const oneWord = function (str) {
   return str.replaceAll(" ", "").toLowerCase();
 };
-// console.log(oneWord("JavaScript is the best!"));
+console.log(oneWord("JavaScript is the best!"));
 
 const upperFirstWord = function (str) {
   const [first, ...others] = str.split(" "); // "..." = the REST Operator used here to PACK all of the remaining words into one Array
   return [first.toUpperCase(), ...others].join(" "); // "..." = the SPREAD Operator used here to UNPACK the "others" Array created earlier with the Rest Operator
 };
-// console.log(upperFirstWord("JavaScript is the best!"));
+console.log(upperFirstWord("JavaScript is the best!"));
 
-// HIGHER-ORDER Function
+// 4. HIGHER-ORDER Function
 const transformer = function (str, fn) {
   console.log(`Original string: ${str}`);
   console.log(`Transformed string: ${fn(str)}`); // CALLING the CALLBACK function ("fn" = "upperFirstWord" / "oneWord")
@@ -99,7 +99,7 @@ transformer("JavaScript is the best!", oneWord);
 // ["Jonas", "Martha", "Adam"].forEach(high5);
 
 ///////////////////////////////////////
-// 4. Functions Returning Functions
+// 5. Functions Returning Functions
 const greet = function (greeting) {
   return function (name) {
     console.log(`${greeting}, ${name}! 👋`);
@@ -245,6 +245,184 @@ const addVAT2 = addTaxRate(0.23);
 console.log(addVAT2(100));
 console.log(addVAT2(23));
 
-/*
+///////////////////////////////////////
+// Coding Challenge #14
+/* 
+Let's build a simple poll app!
 
+A poll has a question, an array of options from which people can choose, and an array with the number of replies for each option. This data is stored in the starter object below.
+
+Here are your tasks:
+
+1. Create a method called 'registerNewAnswer' on the 'poll' object. The method does 2 things:
+  1.1. Display a prompt window for the user to input the number of the selected option. The prompt should look like this:
+        What is your favourite programming language?
+        0: JavaScript
+        1: Python
+        2: Rust
+        3: C++
+        (Write option number)
+  
+  1.2. Based on the input number, update the answers array. For example, if the option is 3, increase the value AT POSITION 3 of the array by 1. Make sure to check if the input is a number and if the number makes sense (e.g answer 52 wouldn't make sense, right?)
+2. Call this method whenever the user clicks the "Answer poll" button.
+3. Create a method 'displayResults' which displays the poll results. The method takes a string as an input (called 'type'), which can be either 'string' or 'array'. If type is 'array', simply display the results array as it is, using console.log(). This should be the default option. If type is 'string', display a string like "Poll results are 13, 2, 4, 1". 
+4. Run the 'displayResults' method at the end of each 'registerNewAnswer' method call.
+
+HINT: Use many of the tools you learned about in this and the last section 😉
+
+BONUS: Use the 'displayResults' method to display the 2 arrays in the test data. Use both the 'array' and the 'string' option. Do NOT put the arrays in the poll object! So what shoud the this keyword look like in this situation?
+
+BONUS TEST DATA 1: [5, 2, 3]
+BONUS TEST DATA 2: [1, 5, 3, 9, 6, 1]
+
+GOOD LUCK 😀
+*/
+
+const poll = {
+  question: "What is your favourite programming language?",
+  options: ["0: JavaScript", "1: Python", "2: Rust", "3: C++"],
+  // This generates [0, 0, 0, 0]. More in the next section 😃
+  answers: new Array(4).fill(0),
+  registerNewAnswer() {
+    // Get answer
+    const answer = Number(
+      prompt(
+        `${this.question}\n${this.options.join("\n")}\n(Write option number)`
+      )
+    );
+    console.log(answer);
+
+    // Register answer
+    typeof answer === "number" &&
+      answer < this.answers.length &&
+      this.answers[answer]++;
+
+    this.displayResults();
+    this.displayResults("string");
+  },
+
+  displayResults(type = "array") {
+    if (type === "array") {
+      console.log(this.answers);
+    } else if (type === "string") {
+      // Poll results are 13, 2, 4, 1
+      console.log(`Poll results are ${this.answers.join(", ")}`);
+    }
+  },
+};
+// poll.registerNewAnswer();
+
+document
+  .querySelector(".poll")
+  .addEventListener("click", poll.registerNewAnswer.bind(poll));
+
+// BONUS:
+// [5, 2, 3]
+// [1, 5, 3, 9, 6, 1]
+poll.displayResults.call({ answers: [5, 2, 3] }, "string");
+poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] }, "string");
+poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] });
+
+///////////////////////////////////////
+// 8. Immediately Invoked Function Expressions (IIFE)
+const runOnce = function () {
+  console.log("This will run again");
+};
+runOnce();
+// we can Call this function anytime
+
+// IIFE Pattern
+(function () {
+  console.log("This will NEVER run again");
+  const isPrivate = 23; // the "isPrivate" variable is ENCAPSULATED INSIDE THE FUNCTION SCOPE, it cannot be accessed from outside the function
+})();
+// RUNS the function ONLY ONCE, IMMEDIATELY after it has been created
+
+// ALSO IIFE
+(() => console.log("This will ALSO never run again"))();
+
+// console.log(isPrivate); // NOT WORKING because of SCOPING
+
+// /after ES6, we can HIDE / PROTECT variables with "let" / "const" which are BLOCK SCOPED
+{
+  const isPrivate = 23; // BLOCK SCOPED - cannot be accessed from outside the Code Block
+  var notPrivate = 46; // HOISTED - it can be accessed from outside the Code Block
+}
+// console.log(isPrivate);
+console.log(notPrivate);
+
+///////////////////////////////////////
+// 9. CLOSURES !!!
+const secureBooking = function () {
+  let passengerCount = 0;
+
+  return function () {
+    passengerCount++;
+    console.log(`${passengerCount} passengers`);
+  };
+};
+
+// console.log(passengerCount); // NOT working because "passengerCount" is BLOCK SCOPED
+
+const booker = secureBooking();
+// secureBooking()();
+// secureBooking()();
+// secureBooking()();
+
+booker(); // WORKS because of CLOSURES
+booker();
+booker();
+
+// to ONLY LOOK at into the CLOSURE / BACKPACK of a function, we use:
+console.dir(booker);
+// "[[...]]" = double brackets mean it's an INTERNAL PROPERTY of a function
+
+///////////////////////////////////////
+// 9.1. More Closure Examples !!!
+// WE DON'T ALWAYS NEED TO RETURN A FUNCTION TO SEE A CLOSURE IN ACTION => we can use Functions RETURNING Functions, CALLBACK Functions ETC.(??) TO SEE A CLOSURE IN ACTION
+// Example 1
+let f; // the "f" Variable is created in the Global Scope
+
+const g = function () {
+  const a = 23;
+  f = function () {
+    // we assigned a function to the "f" Variable => a "f" function was created INSIDE the "g" function
+    console.log(a * 2);
+  };
+};
+
+const h = function () {
+  const b = 777;
+  f = function () {
+    // we assigned a function to the "f" Variable => ANOTHER "f" function was created INSIDE the "g" function
+    console.log(b * 2);
+  };
+};
+
+g(); // the birthplace of the "f" function
+f(); // now the "f" function ALWAYS has ACCESS from it's BACKPACK / CLOSURE to the "a" Variable => "console.log(a(=23) * 2)" => 46
+console.dir(f);
+
+// Re-assigning the "f" function
+h(); // the birthplace of ANOTHER "f" function
+f(); // now the "f" function ALWAYS has ACCESS from it's BACKPACK / CLOSURE to the "b" Variable => "console.log(b(=777) * 2)" => 1554
+console.dir(f);
+
+// Example 2
+const boardPassengers = function (n, wait) {
+  const perGroup = n / 3; // comment out to test CLOSURE PRIORITY over the Scope Chain
+
+  // a CALLBACK function
+  setTimeout(function () {
+    console.log(`We are now boarding all ${n} passengers`);
+    console.log(`There are 3 groups, each with ${perGroup} passengers`);
+  }, wait * 1000);
+
+  console.log(`Will start boarding in ${wait} seconds`);
+};
+
+const perGroup = 1000; // PROOF that the CLOSURE has PRIORITY over the Scope Chain
+boardPassengers(180, 3);
+
+/*
  */
