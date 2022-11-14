d5532cb18a6def4aec427e93cc65f1984c63e137
PRCWebDev <webdevprc@gmail.com>
2022-11-15 00:08:57 +0200

    completed codewars K20

--- js-challenges/codewars-katas-16-20/script.js
+++ js-challenges/codewars-katas-16-20/script.js
@@ -106,3 +106,21 @@ console.log(basicOp('*', 5, 5)); // 25
 console.log(basicOp('/', 49, 7)); // 7
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+console.log('     ---K20---     ');
+/*
+K20:
+Grasshopper - Personalized Message:
+Create a function that gives a personalized greeting. This function takes two parameters: name and owner.
+
+Use conditionals to return the proper message:
+case         	          return
+name equals owner	      'Hello boss'
+otherwise	              'Hello guest'
+*/
+function greet(name, owner) {
+  // Add code here
+  return name === owner ? 'Hello boss' : 'Hello guest';
+}
+console.log(greet('Daniel', 'Daniel')); // 'Hello boss'
+console.log(greet('Greg', 'Daniel')); // 'Hello guest'
