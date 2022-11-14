c6d7adc4f0a1cde57a64e4b0bcc2f91f2cc1825e
PRCWebDev <webdevprc@gmail.com>
2022-11-14 23:26:14 +0200

    completed codewars K17

--- js-challenges/codewars-katas-16-20/script.js
+++ js-challenges/codewars-katas-16-20/script.js
@@ -28,3 +28,23 @@ console.log(summation(2)); // 3
 console.log(summation(8)); // 36
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+console.log('     ---K17---     ');
+/*
+K17:
+Shortest Word:
+Simple, given a string of words, return the length of the shortest word(s).
+String will never be empty and you do not need to account for different data types.
+*/
+function findShort(s) {
+  const sToArr = s.split(' ').map((word) => word.length);
+  const shortestWord = Math.min(...sToArr);
+  return shortestWord;
+}
+console.log(findShort('bitcoin take over the world maybe who knows perhaps')); // 3
+console.log(
+  findShort(
+    'turns out random test cases are easier than writing out basic ones'
+  )
+); // 3
+console.log(findShort("Let's travel abroad shall we")); // 2
