7f9334424baf3a0ffdb02bdc860fb90faf07a7e6
PRCWebDev <webdevprc@gmail.com>
2022-11-14 23:32:17 +0200

    completed codewars K18

--- js-challenges/codewars-katas-16-20/script.js
+++ js-challenges/codewars-katas-16-20/script.js
@@ -48,3 +48,33 @@ console.log(
   )
 ); // 3
 console.log(findShort("Let's travel abroad shall we")); // 2
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+console.log('     ---K18---     ');
+/*
+K18:
+Rock Paper Scissors!
+Let's play! You have to return which player won! In case of a draw return Draw!.
+
+Examples(Input1, Input2 --> Output):
+"scissors", "paper" --> "Player 1 won!"
+"scissors", "rock" --> "Player 2 won!"
+"paper", "paper" --> "Draw!"
+*/
+const rps = (p1, p2) => {
+  if (p1 === 'scissors' && p2 === 'paper') return 'Player 1 won!';
+  if (p1 === 'paper' && p2 === 'rock') return 'Player 1 won!';
+  if (p1 === 'rock' && p2 === 'scissors') return 'Player 1 won!';
+
+  if (p2 === 'scissors' && p1 === 'paper') return 'Player 2 won!';
+  if (p2 === 'paper' && p1 === 'rock') return 'Player 2 won!';
+  if (p2 === 'rock' && p1 === 'scissors') return 'Player 2 won!';
+
+  return 'Draw!';
+};
+console.log(rps('scissors', 'paper')); // "Player 1 won!"
+console.log(rps('scissors', 'rock')); // "Player 2 won!"
+console.log(rps('paper', 'paper')); // "Draw!"
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
